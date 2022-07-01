06dffbcbdce36e1762109ee944b59fa7684b2ff8
vanessawin <vanessa.win@hotmail.com>
2022-06-30 22:49:17 -0300

    ajustando parcelas vindo da api

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-// Baner
-// let slideIndex = 0;
-// showSlides();
+//Baner
 
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";  
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}    
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " active";
-//   setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 
 // Get produtos
 const url = "https://corebiz-test.herokuapp.com/api/v1/products"
@@ -66,12 +67,13 @@ async function getProdutos() {
         ppreco.setAttribute("class", "preco");
         ppreco.innerText = `por R$ ${post.price}`,
 
-            pparcelado.setAttribute("id", "parcelado");
+        pparcelado.setAttribute("id", "parcelado");
         pparcelado.setAttribute("class", "parcelado");
-        pparcelado.innerText = `ou em ${post.installments.quantity}x de R$ ${post.installments.value}`;
+        pparcelado.innerText = `ou em ${post.installments[0].quantity}x de R$ ${post.installments[0].value}`;
 
         botao.setAttribute("id", "btn");
         botao.setAttribute("class", "btn");
+        botao.innerText = " Comprar "
 
         divImagemDoProduto.appendChild(img);
 
